856c0780a83d4e9ff96ab77261149e9702687532
FynnMaarten <fynnmaarten.business@gmail.com>
2020-11-12 23:40:19 +0100

    progress with login and started outsourcing navigation

---
+++ website/src/app/pages/settings/billing-settings/billing-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-billing-settings',
+  templateUrl: './billing-settings.component.html',
+  styleUrls: ['./billing-settings.component.css']
+})
+export class BillingSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ website/src/app/pages/settings/settings-navigation/settings-navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-navigation',
+  templateUrl: './settings-navigation.component.html',
+  styleUrls: ['./settings-navigation.component.css']
+})
+export class SettingsNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
